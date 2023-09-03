f387d74c30ec287754861e53f5d4af6c94f5e510
Nasimul Hasan <nasimulhasandeep@gmail.com>
2023-09-04 00:27:25 +0600

    Loop,Conditional Statements,Function

---
+++ basic_python_1.py
@@ -0,0 +1,35 @@
+# Loop,Conditional Statements,Function
+
+# Loop
+# for loop
+for i in range(5):
+    print(i)
+    
+# while loop
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+    
+# Conditional Statements
+# if
+if True:
+    print("True")
+
+# if else
+if True:
+    print("True")
+else:
+    print("False")
+
+# if elif else
+if True:
+    print("True")
+elif False:
+    print("False")
+else:
+    print("None")
+    
+# Function
+def func():
+    print("Hello")
